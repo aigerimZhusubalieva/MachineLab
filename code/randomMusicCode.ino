8fa5f84133db1a3bc39e64de7223d493569117ab
Aigerim Zhusubalieva <95541608+aigerimZhusubalieva@users.noreply.github.com>
2024-05-09 10:30:24 +0400

    Update randomMusicCode.ino

--- code/randomMusicCode.ino
+++ code/randomMusicCode.ino
@@ -24,7 +24,7 @@
 bool buttonPreviouslyHigh = false; // Flag to track previous state of the button
 bool action = false; // Flag to track if motor is running
 bool buttonState = true;
-int speed = 50;
+int speed = 50; // Speed of the tree
 
 const int numSongs = 5; // Number of songs available
 const char* songNames[numSongs] = {"candy1.mp3", "candy2.mp3", "candy3.mp3", "candy4.mp3", "candy5.mp3"};
@@ -67,7 +67,7 @@ void loop() {
   if (action && !musicPlayer.stopped()) {
     moveServoMotors();
     moveTreeMotor();
-    // change the way neopixels run in loop - use/dont use delay depending on whether this is one time or continuous animation
+
     if(randomIndex==0):
       runNeoPixelAnimation0();
     elif(randomIndex==1):
@@ -78,22 +78,21 @@ void loop() {
       runNeoPixelAnimation3();
     elif(randomIndex==4):
       runNeoPixelAnimation4();
+
   } else if (action && musicPlayer.stopped()) {
     turnOffNeoPixels();
     stopMotor();
     digitalWrite(13, HIGH);
     action = false;
-    // Serial.println("All actions completed!");
+    Serial.println("All actions completed!");
   }
 }
 
 void initServoMotors() {
-  servoMotor1.write(0);  // Set initial position of motor 1 to 0 degrees
-  servoMotor2.write(50); // Set initial position of motor 2 to 180 degrees
-
-  servoMotor1.attach(SERVO_PIN_1);  // Attach motor 1 to pin A2
-  servoMotor2.attach(SERVO_PIN_2);  // Attach motor 2 to pin A3
-
+  servoMotor1.write(0); 
+  servoMotor2.write(50); 
+  servoMotor1.attach(SERVO_PIN_1); 
+  servoMotor2.attach(SERVO_PIN_2); 
   Serial.println("Bunnies: Initial positions done!");
 }
 
@@ -102,7 +101,7 @@ void initTreeMotor(){
   pinMode(MOTOR_IN1_PIN, OUTPUT);
   pinMode(MOTOR_IN2_PIN, OUTPUT);
 
-  digitalWrite(MOTOR_IN1_PIN, LOW); // Set motor direction
+  digitalWrite(MOTOR_IN1_PIN, LOW);
   digitalWrite(MOTOR_IN2_PIN, LOW);
 
   Serial.println("Tree initialized!");
@@ -112,13 +111,9 @@ void startAllActions(randomIndex) {
   Serial.println("Starting all actions...");
   initMusicPlayer();
   playRandomSong(randomIndex);
-  // moveTreeMotor();
-  // moveServoMotors();
 }
 
 void playRandomSong(randomIndex) {
-  // Generate a random index
-  // Play the corresponding song
   Serial.print(F("Playing track "));
   Serial.println(randomIndex + 1);
   // musicPlayer.startPlayingFile("/candy001.mp3");
@@ -132,16 +127,13 @@ void initMusicPlayer() {
   }
   Serial.println("VS1053 found");
   
-  // Initialize the SD card
   if (!SD.begin(CARDCS)) {
     Serial.println("SD failed, or not present");
     while (1);
   }
 
-  // Set volume for left, right channels
   musicPlayer.setVolume(1, 1);
 
-  // Use DREQ interrupt
   musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);
 }
 
@@ -151,11 +143,6 @@ void initNeoPixels() {
   strip.show();
 }
 
-// void playMusic(const char* filename) {
-//   Serial.print("Playing ");
-//   Serial.println(filename);
-//   musicPlayer.startPlayingFile(filename);
-// }
 
 // candy shop
 void runNeoPixelAnimation0() {
@@ -164,14 +151,13 @@ void runNeoPixelAnimation0() {
   if (currentTime - lastUpdate >= animationInterval) {
     lastUpdate = currentTime;
 
-    // Set the color of the current pixel
-    strip.setPixelColor(currentPixel, strip.Color(64, 0, 64)); // Purple color
-    strip.show(); // Update strip with new color
 
-    // Move to the next pixel
+    strip.setPixelColor(currentPixel, strip.Color(64, 0, 64)); // Purple color
+    strip.show();
+
     currentPixel++;
     if (currentPixel >= NUMPIXELS) {
-      currentPixel = 0; // Reset to the beginning once reached the end
+      currentPixel = 0;
     }
   }
 }
@@ -186,7 +172,7 @@ void runNeoPixelAnimation1() {
   for(int i = 24; i < 35; i++) {
     strip.setPixelColor(i, strip.Color(255, 0, 0)); // Red color for last 11 pixels
   }
-  strip.show(); // Update strip with new colors
+  strip.show();
 }
 
 // disney intro
@@ -196,24 +182,20 @@ void runNeoPixelAnimation2() {
   if (currentTime - lastUpdate >= animationInterval) {
     lastUpdate = currentTime;
 
-    // Calculate the progress (0 to 1) of the animation
     float progress = float(currentStep) / float(NUMPIXELS);
 
-    // Calculate the color gradient from white to blue based on the progress
     int red = map(progress, 0, 1, 255, 0); // Gradient from 255 (white) to 0 (blue)
     int green = map(progress, 0, 1, 255, 0); // Gradient from 255 (white) to 0 (blue)
     int blue = 255; // Blue component stays constant (255)
 
-    // Set the color of all pixels
     for (int i = 0; i < NUMPIXELS; i++) {
       strip.setPixelColor(i, strip.Color(red, green, blue));
     }
-    strip.show(); // Update strip with new colors
+    strip.show();
 
-    // Move to the next step
     currentStep++;
     if (currentStep >= NUMPIXELS) {
-      currentStep = 0; // Reset to the beginning once reached the end
+      currentStep = 0;
     }
   }
 }
@@ -221,15 +203,15 @@ void runNeoPixelAnimation2() {
 // small world
 void runNeoPixelAnimation3() {
   for(int i = 0; i < 12; i++) {
-    strip.setPixelColor(i, strip.Color(144, 238, 144)); // Green color for first 12 pixels
+    strip.setPixelColor(i, strip.Color(144, 238, 144));
   }
   for(int i = 12; i < 24; i++) {
-    strip.setPixelColor(i, strip.Color(114, 93, 232)); // White color for next 12 pixels
+    strip.setPixelColor(i, strip.Color(114, 93, 232));
   }
   for(int i = 24; i < 35; i++) {
-    strip.setPixelColor(i, strip.Color(252, 219, 3)); // Red color for last 11 pixels
+    strip.setPixelColor(i, strip.Color(252, 219, 3));
   }
-  strip.show(); // Update strip with new colors
+  strip.show();
 }
 
 // hozier youre too sweet for me
@@ -243,45 +225,34 @@ void runNeoPixelAnimation4() {
   for(int i = 24; i < 35; i++) {
     strip.setPixelColor(i, strip.Color(255, 0, 0)); 
   }
-  strip.show(); // Update strip with new colors
+  strip.show();
 }
 
 void turnOffNeoPixels() {
-  // Set all NeoPixels to black (no light)
   for(int i = 0; i < NUMPIXELS; i++) {
-    strip.setPixelColor(i, strip.Color(0, 0, 0)); // Black color (no light)
+    strip.setPixelColor(i, strip.Color(0, 0, 0));
   }
-  strip.show(); // Update strip with new colors
+  strip.show();
 }
 
 void moveServoMotors() {
-  // Move motor 0 to 15 degrees
   servoMotor1.write(25);
-  
-  // Move motor 2 to 15 degrees (180 - 25)
   servoMotor2.write(50);
-  
-  delay(1200);  // Wait for the servos to reach their positions
-  
-  // Move motor 1 back to 0 degrees
+  delay(1200); 
   servoMotor1.write(0);
-  
-  // Move motor 2 back to 180 degrees
-    // Move motor 2 back to 180 degrees
   servoMotor2.write(25);
-  
-  delay(1200);  // Wait for the servos to reach their positions
+  delay(1200); 
 }
 
 void moveTreeMotor() {
-  analogWrite(MOTOR_ENA_PIN, speed); // Set motor speed
-  digitalWrite(MOTOR_IN1_PIN, LOW); // Set motor direction
+  analogWrite(MOTOR_ENA_PIN, speed);
+  digitalWrite(MOTOR_IN1_PIN, LOW);
   digitalWrite(MOTOR_IN2_PIN, HIGH);
   delay(100);
 }
 
 void stopMotor() {
-  analogWrite(MOTOR_ENA_PIN, 0); // Turn off motor
+  analogWrite(MOTOR_ENA_PIN, 0);
   Serial.println("Motor Stopped");
 }
  
